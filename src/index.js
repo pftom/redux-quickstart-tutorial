d129c5634968cf7740f94679582c55111eafd921
pftom <1043269994@qq.com>
2019-12-01 20:43:38 +0800

    combineReducers: 组合 Reducers 拆分状态

--- src/index.js
+++ src/index.js
@@ -4,70 +4,9 @@ import App, { VisibilityFilters } from "./components/App";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
-const initialState = {
-  todos: [
-    {
-      id: 1,
-      text: "你好, 图雀",
-      completed: false
-    },
-    {
-      id: 2,
-      text: "我是一只小小小小图雀",
-      completed: false
-    },
-    {
-      id: 3,
-      text: "小若燕雀，亦可一展宏图！",
-      completed: false
-    }
-  ],
-  filter: VisibilityFilters.SHOW_ALL
-};
-
-const rootReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_TODO": {
-      const { todos } = state;
-
-      return {
-        ...state,
-        todos: [
-          ...todos,
-          {
-            id: action.id,
-            text: action.text,
-            completed: false
-          }
-        ]
-      };
-    }
-
-    case "TOGGLE_TODO": {
-      const { todos } = state;
-
-      return {
-        ...state,
-        todos: todos.map(todo =>
-          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-        )
-      };
-    }
-
-    case "SET_VISIBILITY_FILTER": {
-      return {
-        ...state,
-        filter: action.filter
-      };
-    }
-
-    default:
-      return state;
-  }
-};
-
-const store = createStore(rootReducer, initialState);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/todos.js
@@ -0,0 +1,43 @@
+const initialTodoState = [
+  {
+    id: 1,
+    text: "你好, 图雀",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "我是一只小小小小图雀",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "小若燕雀，亦可一展宏图！",
+    completed: false
+  }
+];
+
+const todos = (state = initialTodoState, action) => {
+  switch (action.type) {
+    case "ADD_TODO": {
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    }
+
+    case "TOGGLE_TODO": {
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default todos;
