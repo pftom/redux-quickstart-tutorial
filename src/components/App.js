052e81b8246669025bd5c445c768a7ac136d0f4f
pftom <1043269994@qq.com>
2019-12-02 16:17:23 +0800

    重构代码: 将 Footer 的状态和渲染分离

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,16 @@ import AddTodo from "./AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 import Footer from "./Footer";
 
-import { connect } from "react-redux";
-
 class App extends React.Component {
   render() {
-    const { filter } = this.props;
-
     return (
       <div>
         <AddTodo />
         <VisibleTodoList />
-        <Footer filter={filter} />
+        <Footer />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state, props) => ({
-  filter: state.filter
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,14 @@
 import React from "react";
-import Link from "./Link";
+import FilterLink from "../containers/FilterLink";
 import { VisibilityFilters } from "../actions";
 
-import { connect } from "react-redux";
-import { setVisibilityFilter } from "../actions";
-
-const Footer = ({ filter, dispatch }) => (
+const Footer = () => (
   <div>
     <span>Show: </span>
-    <Link
-      active={VisibilityFilters.SHOW_ALL === filter}
-      onClick={() => dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ALL))}
-    >
-      All
-    </Link>
-    <Link
-      active={VisibilityFilters.SHOW_ACTIVE === filter}
-      onClick={() =>
-        dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE))
-      }
-    >
-      Active
-    </Link>
-    <Link
-      active={VisibilityFilters.SHOW_COMPLETED === filter}
-      onClick={() =>
-        dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-      }
-    >
-      Completed
-    </Link>
+    <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
   </div>
 );
 
-export default connect()(Footer);
+export default Footer;
