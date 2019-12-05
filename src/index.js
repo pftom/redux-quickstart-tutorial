1d5bff754d20b0edac8464952e3ec101defc592f
pftom <1043269994@qq.com>
2019-12-05 17:15:48 +0800

    tuture: 修复存在的问题

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App, { VisibilityFilters } from "./components/App";
+import App from "./components/App";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
