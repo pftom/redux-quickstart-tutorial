2fe5a3f1ae83a3806aee6f48e8a4882eff0061cd
gongzelong <gongzelong@meituan.com>
2020-01-10 14:05:25 +0800

    尝试添加默认的待办事项但是运行时报错
    TypeError: todos.map is not a function
    TodoList.render
    src/components/TodoList.js:11

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-let nextTodoId = 0;
+let nextTodoId = 4;
 
 export const addTodo = text => ({
   type: "ADD_TODO",
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,23 @@
-const todos = (state = [], action) => {
+const initialState = [
+  {
+    id: 1,
+    text: "你好, 图雀",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "我是一只小小小小图雀",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "小若燕雀，亦可一展宏图！",
+    completed: false
+  }
+]
+
+
+const todos = (state = {...initialState}, action) => {
   switch (action.type) {
     case "ADD_TODO": {
       return [
