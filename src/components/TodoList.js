38d4b2a65707b708387a63e3fc6409a14a9a3c9a
gongzelong <gongzelong@meituan.com>
2020-01-13 11:45:44 +0800

    正确打印出store.getState()

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,16 @@ import store from "../index";
 class TodoList extends Component {
     render() {
         let {todos, toggleTodo} = this.props;
-        // console.log(store.getState())
+        //文件打包的顺序是有问题的，会调用第一次render的时候store为undefined
+        //增加一个todo后会有值
+        console.log(store)
+        if (typeof(store) === 'undefined') {
+            //当atore不为undefined时，必须加上typeof()，否则还是会走store.getState，然后爆出运行时错误
+            console.log('store is undefined')
+        } else {
+            console.log(store.getState())
+            console.log(store.dispatch)
+        }
         return (
             <ul>
                 {todos.map(todo => (
